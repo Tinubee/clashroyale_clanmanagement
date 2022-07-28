f50a0b2cb6ea983e88b692df3ea98c3e96525412
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-07-28 22:18:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { useRecoilValue } from "recoil";
 import { ThemeProvider } from "styled-components";
-import { modeDarkAtom } from "./atoms";
 import Router from "./routes";
 import { GlobalStyle } from "./styles";
-import { darkMode, lightMode } from "./theme";
+import { darkMode } from "./theme";
 
 function App() {
-  const modeDark = useRecoilValue(modeDarkAtom);
   return (
     <>
-      <ThemeProvider theme={modeDark ? darkMode : lightMode}>
+      <ThemeProvider theme={darkMode}>
         <GlobalStyle />
         <Router />
       </ThemeProvider>
